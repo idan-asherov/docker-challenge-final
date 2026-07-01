232069adb2ef9ddca56c2dcbf2e05caa418a1c28
“Idan”-asherov” <Idan.asherov@gmail.com>
2026-07-01 22:57:20 +0300

    Initial commit: Clean unified src architecture

---
+++ src/controllers/users.controller.js
@@ -0,0 +1,23 @@
+const User = require("../model/User");
+
+// Get all users from the database
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Create and save a new user
+exports.createUser = async (req, res) => {
+  const { name, email } = req.body;
+  try {
+    const newUser = new User({ name, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
---
+++ src/routes/users.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const { getUsers, createUser } = require("../controllers/users.controller");
+
+router.route("/")
+  .get(getUsers)
+  .post(createUser);
+
+module.exports = router;
